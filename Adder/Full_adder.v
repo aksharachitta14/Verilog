62bc8d1104039068c305e6f40e70ae86e2edf5b2
Chitta Akshara <chittaakshara14@gmail.com>
2026-07-06 22:41:12 +0530

    Update Full_adder.v

--- Adder/Full_adder.v
+++ Adder/Full_adder.v
@@ -23,11 +23,11 @@ module full_adder(a,b,c,sum,carry);
 	input a,b,c;
 	output sum,carry;
 	wire w1,w2,w3;
-	xor(sum,a,b,c);
-	and(w1,a,b);
-	and(w2,b,c);
-	and(w3,c,a);
-	or(carry,w1,w2,w3);
+	xor g1(sum,a,b,c);
+	and g2(w1,a,b);
+	and g3(w2,b,c);
+	and g4(w3,c,a);
+	or g5(carry,w1,w2,w3);
 endmodule
 
 //TestBench
@@ -38,7 +38,18 @@ module top;
 	initial begin
 		repeat(8)begin
 			{a,b,c}=$random;
+			#1;
 			$display("A=%b B=%b C=%b Sum=%b Carry=%b",a,b,c,sum,carry);
 		end
 	end
 endmodule
+
+/*output
+# A=1 B=0 C=0 Sum=1 Carry=0
+# A=0 B=0 C=1 Sum=1 Carry=0
+# A=0 B=0 C=1 Sum=1 Carry=0
+# A=0 B=1 C=1 Sum=0 Carry=1
+# A=1 B=0 C=1 Sum=0 Carry=1
+# A=1 B=0 C=1 Sum=0 Carry=1
+# A=1 B=0 C=1 Sum=0 Carry=1
+# A=0 B=1 C=0 Sum=1 Carry=0*/
